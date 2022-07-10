6841962bc6e4cfeff22015b749485da717a94036
Mikadev1996 <charmikadev@gmail.com>
2022-07-10 23:10:16 +0100

    thp create post array

--- controllers/thpController.js
+++ controllers/thpController.js
@@ -42,23 +42,37 @@ exports.thp_detail = (req, res, next) => {
         })
 }
 
-exports.thp_create_get = (req, res, next) => [
-    body('name', 'Name must not be empty.').trim.isLength({min: 1}).escape(),
-    body('release_date', 'Release Date must not be empty.').trim.isLength({min: 1}).escape(),
-    body('number_in_stock', 'Number of Devices must not be empty.').trim.isLength({min: 1}).escape(),
-    body('consumable', 'Consumable Type must not be empty.').trim.isLength({min: 1}).escape(),
+exports.thp_create_get = (req, res, next) => {
+    res.render('thp_create', {title: "Add your Device"})
+}
+
+exports.thp_create_post = [
+    body('name', 'Name must not be empty.').trim().isLength({min: 1}).escape(),
+    body('release_date', 'Release Date must not be empty.').trim().isLength({min: 1}).escape(),
+    body('number_in_stock', 'Number of Devices must not be empty.').trim().isLength({min: 1}).escape(),
+    body('consumable', 'Consumable Type must not be empty.').trim().isLength({min: 1}).escape(),
 
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-
+            console.log('errrr');
+            return;
         }
+
+        let thp = new Thp({
+            name: req.body.name,
+            release_date: req.body.release_date,
+            number_in_stock: req.body.number_in_stock,
+            consumable: req.body.consumable
+        })
+
+        thp.save((err) => {
+            if (err) return next(err);
+            res.redirect('/category/thp');
+        })
     }
 ]
 
-exports.thp_create_post = (req, res, next) => {
-    res.send('Not Implemented yet');
-}
 
 exports.thp_delete_get = (req, res, next) => {
     res.send('Not Implemented yet');
